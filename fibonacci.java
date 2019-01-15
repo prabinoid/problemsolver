c92d87290964ec4a0b90ee4918754e49a4061499
prabinoid <38830224+prabinoid@users.noreply.github.com>
2019-01-15 15:57:29 +0545

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,33 @@
+public class Prabin  
+{ 
+    static int sumevenFib(int limit) 
+    { 
+        if (limit < 2) 
+            return 0; 
+      
+        long preceeding = 0, trailing = 2; 
+        long sum = preceeding + trailing; 
+      
+        while (trailing <= limit) 
+        { 
+            long sum_current = 4 * trailing + preceeding; 
+      
+            if (sum_current > limit) 
+                break; 
+      
+            preceeding = trailing; 
+            trailing = sum_current; 
+            sum += trailing; 
+        } 
+      
+        return(int) sum; 
+    } 
+      
+    public static void main (String[] args)  
+    { 
+        int limit = 4000000; 
+        System.out.println(sumevenFib(limit)); 
+          
+    } 
+} 
+  
